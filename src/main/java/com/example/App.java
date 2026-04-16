728a9e6ceabec55589c4592d5b99551e61eb84d8
Achintya088 <itagiachintya7@gmail.com>
2026-04-16 14:23:42 +0000

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -7,27 +7,24 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class App {
     public static void main(String[] args) {
 
-        // Set Chrome options for Jenkins
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
+        options.addArguments("--disable-software-rasterizer");
+        options.addArguments("--remote-debugging-port=9222");
 
-        // ✅ FIX: correct Chrome binary path
         options.setBinary("/usr/bin/google-chrome-stable");
 
-        // Initialize driver
         WebDriver driver = new ChromeDriver(options);
 
         try {
             driver.get("https://www.google.com");
-
-            // Print title (for Jenkins console)
             System.out.println("Page Title: " + driver.getTitle());
-
             Thread.sleep(3000);
-
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
